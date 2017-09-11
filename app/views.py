896dc99b55d512f5a1b9df70391905b3ddd8651a
timp21337 <timp21337@paneris.org>
2017-09-11 23:17:42 +0100

    Onwards

--- app/views.py
+++ app/views.py
@@ -1,7 +1,10 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
-
+    user = {'nickname': 'Mr Leeman'}  # fake user
+    return render_template('index.html',
+                           title='Home',
+                           user=user)
